2ee84f2c3f51ceb49241058bb45e6c3cfddd21d7
Tarunkr89 <it.tarun07@gmail.com>
2020-06-24 17:13:14 +0530

    With context api

--- src/context/PlayerContextProvider.js
+++ src/context/PlayerContextProvider.js
@@ -1,9 +1,85 @@
-import React, { createContext } from "react";
-
+import React, { createContext, useState, useCallback } from "react";
+import data from "../api.json";
 export const PlayerContext = createContext();
 
 const PlayerContextProvider = ({ children }) => {
-  return <PlayerContext.Provider>{children}</PlayerContext.Provider>;
+  const [selectedPlayers, setSelectedPlayers] = useState([]);
+
+  const handleChange = useCallback(
+    (countryId, countryName, player) => {
+      let selectedCountry = selectedPlayers.find(
+        (country) => Number(country.countryId) === Number(countryId)
+      );
+      if (selectedCountry) {
+        if (
+          selectedCountry.players.some(
+            (players) => Number(players.id) === Number(player.id)
+          )
+        ) {
+          selectedCountry.players = selectedCountry.players.filter(
+            (players) => Number(players.id) !== Number(player.id)
+          );
+        } else {
+          selectedCountry.players.push(player);
+        }
+        setSelectedPlayers([...selectedPlayers]);
+      } else {
+        setSelectedPlayers([
+          ...selectedPlayers,
+          { countryId, countryName, players: [player] },
+        ]);
+      }
+    },
+    [selectedPlayers, setSelectedPlayers]
+  );
+
+  const verifyCheck = useCallback(
+    (id, countryId) => {
+      if (selectedPlayers) {
+        let selectedCountry = selectedPlayers.find(
+          (country) => Number(country.countryId) === Number(countryId)
+        );
+        if (selectedCountry) {
+          const selected = selectedCountry.players.some(
+            (player) => Number(player.id) === Number(id)
+          );
+          return selected;
+        }
+      }
+      return false;
+    },
+    [selectedPlayers]
+  );
+
+  const removePlayerHandler = useCallback(
+    (id, countryId) => {
+      let selectedCountry = selectedPlayers.find(
+        (country) => Number(country.countryId) === Number(countryId)
+      );
+      if (selectedCountry && selectedCountry.players.length > 0) {
+        selectedCountry.players = selectedCountry.players.filter(
+          (player) => Number(player.id) !== Number(id)
+        );
+        verifyCheck(id, countryId);
+        setSelectedPlayers([...selectedPlayers]);
+      }
+    },
+    [selectedPlayers, setSelectedPlayers, verifyCheck]
+  );
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        selectedPlayers,
+        handleChange,
+        removePlayerHandler,
+        verifyCheck,
+        data,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
 };
 
 export default PlayerContextProvider;
--- src/pages/Players.js
+++ src/pages/Players.js
@@ -1,108 +1,54 @@
-import React, { useState, useCallback } from "react";
+import React, { useContext } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import data from "../api.json";
 import SelectedPlayerList from "../components/SelectedPlayerList";
+import { PlayerContext } from "../context/PlayerContextProvider";
 
 const Players = () => {
-  const [selectedPlayers, setSelectedPlayers] = useState([]);
-
-  const handleChange = useCallback(
-    (countryId, countryName, player) => {
-      let selectedCountry = selectedPlayers.find(
-        (country) => Number(country.countryId) === Number(countryId)
-      );
-      if (selectedCountry) {
-        if (
-          selectedCountry.players.some(
-            (players) => Number(players.id) === Number(player.id)
-          )
-        ) {
-          selectedCountry.players = selectedCountry.players.filter(
-            (players) => Number(players.id) !== Number(player.id)
-          );
-        } else {
-          selectedCountry.players.push(player);
-        }
-        setSelectedPlayers([...selectedPlayers]);
-      } else {
-        setSelectedPlayers([
-          ...selectedPlayers,
-          { countryId, countryName, players: [player] },
-        ]);
-      }
-    },
-    [selectedPlayers, setSelectedPlayers]
-  );
-
-  const verifyCheck = useCallback(
-    (id, countryId) => {
-      if (selectedPlayers) {
-        let selectedCountry = selectedPlayers.find(
-          (country) => Number(country.countryId) === Number(countryId)
-        );
-        if (selectedCountry) {
-          const selected = selectedCountry.players.some(
-            (player) => Number(player.id) === Number(id)
-          );
-          return selected;
-        }
-      }
-      return false;
-    },
-    [selectedPlayers]
-  );
-
-  const removePlayerHandler = useCallback(
-    (id, countryId) => {
-      let selectedCountry = selectedPlayers.find(
-        (country) => Number(country.countryId) === Number(countryId)
-      );
-      if (selectedCountry && selectedCountry.players.length > 0) {
-        selectedCountry.players = selectedCountry.players.filter(
-          (player) => Number(player.id) !== Number(id)
-        );
-        verifyCheck(id, countryId);
-        setSelectedPlayers([...selectedPlayers]);
-      }
-    },
-    [selectedPlayers, setSelectedPlayers, verifyCheck]
-  );
+  const {
+    selectedPlayers,
+    handleChange,
+    removePlayerHandler,
+    verifyCheck,
+    data,
+  } = useContext(PlayerContext);
 
   return (
     <div className="bg">
-      <Container>
+      <Container className="pt-5">
         <Row>
-          <Col className="card" md={6}>
-            {data.data.country.map((country) => (
-              <form key={country.countryId}>
-                <h3>{country.name}</h3>
-                <ul>
-                  {country.players.map((player) => {
-                    return (
-                      <li key={player.id}>
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          {...(verifyCheck(player.id, country.countryId)
-                            ? { checked: "true" }
-                            : {})}
-                          onChange={() =>
-                            handleChange(
-                              country.countryId,
-                              country.name,
-                              player
-                            )
-                          }
-                        />
-                        <span>{player.name}</span>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </form>
-            ))}
+          <Col md={6}>
+            <Col className="card mr-3" md={12}>
+              {data.data.country.map((country) => (
+                <form key={country.countryId}>
+                  <h3>{country.name}</h3>
+                  <ul>
+                    {country.players.map((player) => {
+                      return (
+                        <li key={player.id}>
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            {...(verifyCheck(player.id, country.countryId)
+                              ? { checked: "true" }
+                              : {})}
+                            onChange={() =>
+                              handleChange(
+                                country.countryId,
+                                country.name,
+                                player
+                              )
+                            }
+                          />
+                          <span>{player.name}</span>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </form>
+              ))}
+            </Col>
           </Col>
-          <Col className="card" md={6}>
+          <Col className="card" md={{ span: 6 }}>
             <SelectedPlayerList
               data={selectedPlayers}
               removePlayerHandler={removePlayerHandler}
